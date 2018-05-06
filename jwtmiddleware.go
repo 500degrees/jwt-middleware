811e5a133c51e229ee3793b36241150995a523c1
Ovidio Calvet <calveto@gmail.com>
2018-05-06 00:03:48 -0400

    add logging and aborts

--- jwtmiddleware.go
+++ jwtmiddleware.go
@@ -2,6 +2,7 @@ package jwtmiddleware
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
@@ -11,33 +12,45 @@ import (
 
 // New creates a jwtmiddleware to parse JWT tokens from the Authorization header
 func New(secret string) gin.HandlerFunc {
+	log.Println("creating middleware with secret", secret)
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
 		parts := strings.Split(authHeader, " ")
+		if len(parts) <= 1 {
+			log.Println("Authorization header not present")
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
+			})
+			c.Abort()
+		}
 		tokenString := parts[1]
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			// Don't forget to validate the alg is what you expect:
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				log.Println("wrong signing method")
 				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 			}
 
 			return []byte(secret), nil
 		})
 		if err != nil {
-			fmt.Println(err)
+			log.Printf("error parsing token: %v", err)
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"message": "Unauthorized",
 			})
+			c.Abort()
 		}
 
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			log.Printf("setting claims: %v", claims)
 			c.Set("Claims", claims)
 			c.Next()
 		} else {
-			fmt.Println(err)
+			log.Println("something weird: %v", err)
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"message": "Unauthorized",
 			})
+			c.Abort()
 		}
 	}
 }
